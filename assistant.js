6da318b4f9169771693a175cab600c876c810b7b
Mike Chen <CHEN_MIKE_C@STUDENT.SMC.EDU>
2026-07-11 20:39:13 -0700

    Create PersonalAssistant constructor

---
+++ assistant.js
@@ -0,0 +1,11 @@
+//https://github.com/chen21414/cs81-module6b-assistant
+function PersonalAssistant(name) {
+  //stores the name 
+  this.name = name;
+
+  //stores array
+  this.tasks = [];
+
+  this.mood = "productive";
+}
+
